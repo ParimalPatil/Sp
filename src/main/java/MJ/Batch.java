4250d2609da7a425c02b69956b83b55cde3eb3cf
ParimalPatil <patilparimal6@gmail.com>
2019-09-16 09:24:06 +0530

    hii master

---
+++ src/main/java/MJ/Batch.java
@@ -0,0 +1,8 @@
+package MJ;
+
+public class Batch {
+		public void m1()
+		{
+			System.out.println("Hello");
+		}
+}
---
+++ src/main/java/com/cjc/Simple3Application.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simple3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simple3Application.class, args);
+	}
+
+}
